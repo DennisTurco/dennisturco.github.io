dad4f5b1c8341717d78fcb5b918db9951e711d47
Dennis Turco <dennisturco@gmail.com>
2025-03-27 12:53:48 +0100

    static sitemap

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+const baseUrl = "https://dennisturco.github.io";
+
+const pages = [
+  "",
+  "progetti",
+  "carriera",
+  "studi",
+];
+
+const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
+    .map(
+      (page) => `
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `
+    )
+    .join("")}
+</urlset>`;
+
+const filePath = path.join(__dirname, "../public/sitemap.xml");
+fs.writeFileSync(filePath, sitemapContent);
+
+console.log("Sitemap generata con successo!");
